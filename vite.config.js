7084379026817f1904dcb452887faec0c62e992b
Charanvs3 <2200031706@kluniversity.in>
2025-04-11 15:57:45 +0530

    Set base path for deployment

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: './', // 👈 Add this line for proper path resolution after deployment
 })
